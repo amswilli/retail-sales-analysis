4b905e3eaacb15af19a9a79fd2b021559bec8938
amswilli <olimpiawilson88@gmail.com>
2024-10-08 22:12:38 -0300

    Updated main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
 import numpy as np
 
 def cargar_datos(ruta_archivo):
-    # Carga los datos del archivo CSV utilizando NumPy
-    datos = np.genfromtxt(ruta_archivo, delimiter=',', skip_header=1)
-    return datos
+    try:
+        datos = np.genfromtxt(ruta_archivo, delimiter=',', skip_header=1)
+        return datos
+    except FileNotFoundError:
+        print(f"El archivo no se encontró: {ruta_archivo}")
+        return None
 
 if __name__ == "__main__":
-    ruta_archivo = '../data/retail_sales.csv'
+    ruta_archivo = "C:/Users/PC Master Race/retail-sales-analysis/data/retail_sales_dataset.csv"
     datos = cargar_datos(ruta_archivo)
-    print(datos)
+    if datos is not None:
+        print(datos)
